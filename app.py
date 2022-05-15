870ced199cff40f5dd627d73b298459936967c7f
Tirtharaj Sinha <sinhatirtharaj@gmail.com>
2022-05-15 19:16:45 +0530

    final app

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ def send_news_update(mails):
         sender = sender()
         sender.get_connection("tirtharaj.ubuntu@gmail.com", "ubuntu098")
         feed = sender.send_mail(mailid, "Today's news", message, "NewsUpdate")
-        feedback += mailid + " : " + feed + "\n"
+        feedback += mailid + " : " + str(feed) + "\n"
     admin = "sinhatirtharaj@gmail.com"
     sender.get_connection("tirtharaj.ubuntu@gmail.com", "ubuntu098")
     feed = sender.send_mail(admin, "Feedback from SchedulerApp", feedback, "schedulerApp")
@@ -28,8 +28,12 @@ def timed_job_m8():
     from mail_sender import services
     services.keep_awake_site("https://tirtharajsinha.herokuapp.com")
 
+@sched.scheduled_job('interval', minutes=5)
+def scheduled_job():
+    mails = ["sinhatirtharaj@gmail.com", "anuragunnikannan7@gmail.com"]
+    send_news_update(mails)
 
-@sched.scheduled_job('cron', day_of_week='*', hour=13, minute=30)
+@sched.scheduled_job('cron', day_of_week='*', hour=14, minute=30)
 def scheduled_job():
     mails = ["sinhatirtharaj@gmail.com", "anuragunnikannan7@gmail.com"]
     send_news_update(mails)
