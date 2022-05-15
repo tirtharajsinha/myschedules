ee0e859f8d60ef6be60c5a3c5dc6d2ffb29b0d35
Tirtharaj Sinha <sinhatirtharaj@gmail.com>
2022-05-15 16:03:02 +0530

    added app

--- app.py
+++ app.py
@@ -1,9 +1,34 @@
-from mail_sender import sender, services
+from apscheduler.schedulers.blocking import BlockingScheduler
 
-sender = sender()
-sender.get_connection("tirtharaj.ubuntu@gmail.com", "ubuntu983221")
+sched = BlockingScheduler()
 
-# message = services.keep_awake_site("https://tirtharajsinha.herokuapp.com")
 
-message=services.newsService()
-sender.send_mail("sinhatirtharaj@gmail.com", "Today's news", message, "random user")
+def send_news_update(mails):
+    from mail_sender import sender, services
+    sender = sender()
+    sender.get_connection("tirtharaj.ubuntu@gmail.com", "ubuntu983221")
+
+    message = services.newsService()
+    for mailid in mails:
+        sender.send_mail(mailid, "Today's news", message, "random user")
+
+
+@sched.scheduled_job('interval', minutes=3)
+def timed_job_m3():
+    mails = ["sinhatirtharaj@gmail.com"]
+    send_news_update(mails)
+
+
+@sched.scheduled_job('interval', minutes=8)
+def timed_job_m8():
+    from mail_sender import services
+    services.keep_awake_site("https://tirtharajsinha.herokuapp.com")
+
+
+@sched.scheduled_job('cron', day_of_week='mon-fri', hour=17)
+def scheduled_job():
+    mails = ["sinhatirtharaj@gmail.com"]
+    send_news_update(mails)
+
+
+sched.start()
